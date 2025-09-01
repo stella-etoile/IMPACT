43feb7f117d40edf254662c95f49c030d85d3961
stella-etoile <stella@writteninbyeol.com>
2025-09-01 02:46:14 -0500

    Added necessary files and removed .DS_Store parse

---
+++ aux/run_gen_gamd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage: ./run_gen_gamd.sh <script_dir> <prefix> <trial_num>
+
+# Arguments
+script_dir=$1
+prefix=$2
+trial_num=$3
+
+# Derived values
+full_prefix="${prefix}_${trial_num}"
+pbc_file="${full_prefix}_pbc.txt"
+
+# Run the command
+"$script_dir/gen_gamd_scripts.sh" "$full_prefix" "$pbc_file"
