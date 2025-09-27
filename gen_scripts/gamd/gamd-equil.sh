c881b26a85ad78e253e049cd65f293c019ef03b5
Kanghee Lee <kangheelee@midway3-login3.rcc.local>
2025-09-27 11:43:24 -0500

    commit

--- gen_scripts/gamd/gamd-equil.sh
+++ gen_scripts/gamd/gamd-equil.sh
@@ -8,12 +8,10 @@
 #SBATCH --ntasks-per-node=48
 #SBATCH --account=pi-haddadian
 
-
 ulimit -l unlimited
 
-
 # module load namd
-module load namd/2.14+intel-2022.0+cuda-11.5
+module load namd/3.0.1-multicore-cuda
 
 # mpiexec.hydra -bootstrap=slurm namd2 "TCR_##-gamd-equil.conf" > "TCR_##-gamd-equil.log"
-charmrun +idlepoll +p32 +setcpuaffinity namd2 +devices 0,1,2,3 'TCR_##-gamd-equil.conf'  > 'TCR_##-gamd-equil.log'
+$NAMD_HOME/namd3 +p 32 +devices 0,1,2,3 +setcpuaffinity 'TCR_##-gamd-equil.conf'  > 'TCR_##-gamd-equil.log'
--- gen_scripts/gamd/gamd-prod.sh
+++ gen_scripts/gamd/gamd-prod.sh
@@ -11,6 +11,8 @@
 #SBATCH --output=TCR_##-npt1.out
 #SBATCH --error=TCR_##-npt1.err
 
-module load namd/2.14
+# module load namd/2.14
+module load namd/3.0.1-multicore-cuda
 
-mpiexec.hydra -bootstrap=slurm namd2 "TCR_##-gamd-npt1.conf" > "TCR_##-gamd-npt1.log"
+# mpiexec.hydra -bootstrap=slurm namd2 "TCR_##-gamd-npt1.conf" > "TCR_##-gamd-npt1.log"
+$NAMD_HOME/namd3 +p 32 +devices 0,1,2,3 +setcpuaffinity 'TCR_##-gamd-npt1.conf' > 'TCR_##-gamd-npt1.log'
